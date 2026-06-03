214d10b509aa74acc25a6a8bdfa855a169689615
frand odi <frandkampus99@gmail.com>
2026-06-03 20:01:34 +0700

    test adding feature

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::get('/hello', function () {
+        return response()->json([
+            'message' => 'Hello, World!',
+        ]);
+    });
+});
+
